1a700d9ecffb4ae3baaf06331e6331d34ed230c6
Clovis Caface <clovis.caface@gmail.com>
2022-11-23 13:20:41 -0300

    create test and fix some errors

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Users
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("name", "username", "email")
+
+admin.site.register(Users, UserAdmin)
--- apps/users/models.py
+++ apps/users/models.py
@@ -18,19 +18,25 @@ class Users(AbstractBaseUser, PermissionsMixin):
             default=uuid.uuid4,
             editable=False,
         )
+    username = models.CharField('Login', max_length=30, default="", unique=True)
     name = models.CharField('Nome', max_length=150, default='')
     email = models.EmailField('Email', unique=True)
 
     is_staff = models.BooleanField('Equipe', default=False)
     is_active = models.BooleanField('Ativo', default=False)
 
-    date_joined = models.DateTimeField(default=timezone.now)
-    
+    date_joined = models.DateTimeField(auto_now_add=True)
+    date_last_modified = models.DateTimeField(auto_now=True)
 
-    USERNAME_FIELD = 'id'
+    USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['name', 'email']
 
     objects = CustomUserManager()
+
+    class Meta:
+        verbose_name = 'Usuário'
+        verbose_name_plural = 'Usuários'
+        ordering = ['name']
 
     def __str__(self) -> str:
         return self.name    
