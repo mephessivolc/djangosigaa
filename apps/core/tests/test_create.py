c33fa6bf1edb9737c628fd68d536bdcc08f3c930
Clovis Caface <clovis.caface@gmail.com>
2022-11-27 11:29:50 -0300

    create departments and courses models

--- apps/core/tests/test_create.py
+++ apps/core/tests/test_create.py
@@ -1,13 +1,13 @@
 from django.test import TestCase
+from django.template.defaultfilters import slugify
 
 # Create your tests here.
 from apps.core import models
 
-class CoreManagersTests(TestCase):
+class InstituteManagersTests(TestCase):
 
     @classmethod
     def setUpTestData(self):
-        print("Teste do app core")
         self.institute = models.Institute.objects.create(
             name = "Nome de Instiução",
             short_name = "NI"
@@ -16,14 +16,52 @@ class CoreManagersTests(TestCase):
     def test_create_institute(self):
         self.assertEqual(self.institute.name, "Nome de Instiução")
         self.assertEqual(self.institute.short_name, "NI")
+        self.assertEqual(self.institute.slug, slugify("Nome de Instiução"))
 
-    def test_create_department(self):
-        department = models.Departaments.objects.create(
+class DepartmentsManagersTests(TestCase):
+
+    @classmethod
+    def setUpTestData(self):
+        self.institute = models.Institute.objects.create(
+            name = "Nome de Instiução",
+            short_name = "NI"
+        )
+
+        self.department = models.Departaments.objects.create(
             name = "Nome departamento",
             short_name = "Dep",
             institute = self.institute
         )
 
-        self.assertEqual(department.name, "Nome departamento")
-        self.assertEqual(department.short_name, "Dep")
-        self.assertEqual(department.institute, self.institute)
+
+    def test_create_department(self):
+
+        self.assertEqual(self.department.name, "Nome departamento")
+        self.assertEqual(self.department.short_name, "Dep")
+        self.assertEqual(self.department.institute, self.institute)
+        self.assertEqual(self.department.slug, slugify("Nome departamento"))
+
+class CoursesManagersTests(TestCase):
+
+    @classmethod
+    def setUpTestData(self):
+        self.institute = models.Institute.objects.create(
+            name = "Nome de Instiução",
+            short_name = "NI"
+        )
+
+        self.department = models.Departaments.objects.create(
+            name = "Nome departamento",
+            short_name = "Dep",
+            institute = self.institute
+        )
+
+        self.course = models.Courses.objects.create(
+            name = "Nome do Curso",
+            departament = self.department
+        )
+
+    def test_create_courses(self):
+        self.assertEqual(self.course.name, "Nome do Curso")
+        self.assertEqual(self.course.slug, slugify("Nome do Curso"))
+        self.assertEqual(self.course.departament, self.department)
