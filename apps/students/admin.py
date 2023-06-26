cf8cffb75cc700e7db6570947dcd7d99c9299a13
Clovis Caface <clovis.caface@gmail.com>
2023-06-25 23:30:03 -0300

    Created admin manager for students.

--- apps/students/admin.py
+++ apps/students/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from apps.students.models import StudentsModel
 # Register your models here.
+
+@admin.register(StudentsModel)
+class StudentsAdmin(admin.ModelAdmin):
+    pass
